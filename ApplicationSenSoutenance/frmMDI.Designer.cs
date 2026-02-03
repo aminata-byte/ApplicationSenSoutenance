e748dc21721d9056d781f5b302dd7e3d4d199b5a
bettyhubcod <ndiayebetty333@gmail.com>
2026-02-03 12:28:58 +0000

    finale

--- ApplicationSenSoutenance/frmMDI.Designer.cs
+++ ApplicationSenSoutenance/frmMDI.Designer.cs
@@ -79,6 +79,7 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(356, 1137);
             this.panel1.TabIndex = 28;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // label3
             // 
